f477e4ee8936187ab7768ba82242a034316aaaed
Dewald Retief <dewret@gmail.com>
2017-03-02 09:21:07 +0000

    Re-installed the bot

--- src/helpers/strings.js
+++ src/helpers/strings.js
@@ -1,12 +1,23 @@
 module.exports = {
   queryString: [
-    ''
+    'iot',
+    'linux',
+    'machine learning',
+    'devops',
+    'golang',
+    'gohugo',
+    'raspberry pi',
+    'big data'
   ],
   queryStringSubQuery: [
-    ''
+    ' ',
+    ' best practice',
+    ' tutorial',
+    ' how to'
   ],
   blockedStrings: [
-    ''
+    'porn',
+    'sex'
   ],
   resultType: [
     'mixed',
@@ -14,6 +25,6 @@ module.exports = {
     'popular'
   ],
   responseString: [
-    'Hi @screenName thanks for the following!'
+    'Hi @screenName thanks for following!'
   ]
 }
